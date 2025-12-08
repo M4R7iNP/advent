3c5f1c46fabded2a229ebf79da6b319252ace48c
Martin Pedersen <martin@m4r7.in>
2025-12-08 08:10:01 +0100

    day2 part1

---
+++ 2025/2/part1.c
@@ -0,0 +1,52 @@
+#include <regex.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef unsigned long MARTIN_NUMBER;
+
+int main() {
+    const char *filename = "input.txt";
+    regex_t regex;
+    FILE *file;
+    char line[1024];
+    MARTIN_NUMBER result = 0;
+
+    if (regcomp(&regex, "^\\([0-9]\\+\\)\\1$", 0) != 0) {
+        fprintf(stderr, "Could not compile regex\n");
+        return 1;
+    }
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file %s\n", filename);
+        regfree(&regex);
+        return 1;
+    }
+
+    char *saveptr;
+
+    fgets(line, sizeof(line), file);
+    char *token;
+    for (token = strtok_r(line, ",", &saveptr); token != NULL; token = strtok_r(NULL, ",", &saveptr)) {
+        // printf("Token: %s\n", token);
+        MARTIN_NUMBER first_num, second_num;
+        sscanf(token, "%lu-%lu", &first_num, &second_num);
+
+        printf("First num: %lu, Second num: %lu\n", first_num, second_num);
+        for (MARTIN_NUMBER i = first_num; i <= second_num; i++) {
+            char num[16];
+            sprintf(num, "%lu", i);
+            if (regexec(&regex, num, 0, NULL, 0) == 0) {
+                result += i;
+            }
+        }
+    }
+
+    fclose(file);
+    regfree(&regex);
+
+    printf("Result: %lu\n", result);
+    return 0;
+}
+
